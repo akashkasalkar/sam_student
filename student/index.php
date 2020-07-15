841af196852d8b7c634269534e489ce6782677f1
akashkasalkar <kasalkar16@gmail.com>
2020-07-16 01:01:41 +0530

    second commit

--- student/index.php
+++ student/index.php
@@ -48,6 +48,27 @@ function getCity(val) {
         
 </script>
 
+</script>
+<script type="text/javascript">
+(function() {
+'use strict';
+window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation');
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+form.addEventListener('submit', function(event) {
+if (form.checkValidity() === false) {
+event.preventDefault();
+event.stopPropagation();
+}
+form.classList.add('was-validated');
+}, false);
+});
+}, false);
+})();
+</script>
+
 </head>
 <body>
 	<div class="container">
@@ -124,6 +145,17 @@ function getCity(val) {
 					  	<label for="cpassword">Confirm Password</label>
 					  	<input type="password" name="cpassword" id="cpassword" class="form-control" required="" placeholder="*****">
 					  </div>
+					  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Agree to terms and conditions
+      </label>
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
+    </div>
+    </div> 
 					  <div class="form-group">
 					  	<button class="btn btn-primary" name="register">Register</button>
 					  	<button class="btn btn-danger" name="clear" type="reset">Clear</button>
